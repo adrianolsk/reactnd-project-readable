a83be23d5f70d87c9d9c011d31309c9c55d60f02
adriano <adriano.skroch@gmail.com>
2017-09-24 23:49:27 -0300

    feat: Create form to add a new post

--- frontend/src/pages/PostForm.js
+++ frontend/src/pages/PostForm.js
@@ -4,19 +4,55 @@ import {createPostsAsync} from "../actions/posts";
 
 class PostForm extends Component {
 
-    save = () => {
-        this.props.createPost({
-            title: 'Test',
-            body: 'Hello',
-            author: 'Adriano',
-            category: 'React'
-        });
+    constructor() {
+        super();
+        // Form inputs and controlled components may have some state handled by the component. (from udacity rubric review)
+        this.state = {
+            title: '',
+            body: '',
+            author: '',
+            category: ''
+        };
     }
 
+    setValue = (event) => {
+        var object = {};
+        object[event.target.id] = event.target.value;
+        this.setState(object);
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        this.props.createPost({...this.state});
+    }
 
     render() {
-        return (<div>Form post
-            <button onClick={this.save}>salvar</button>
+        console.log("State:", this.state);
+
+        return (<div>
+            <form onSubmit={this.handleSubmit}>
+
+                <label htmlFor="title">Title</label>
+                <input id="title" type="text" onChange={(e) => this.setValue(e)}/>
+
+                <label htmlFor="body">Body</label>
+                <textarea id="body" onChange={(e) => this.setValue(e)}/>
+
+                <label htmlFor="author">Author</label>
+                <input id="author" type="text" onChange={(e) => this.setValue(e)}/>
+
+                <label htmlFor="category">Category</label>
+                <select name="" id="category" onChange={(e) => this.setValue(e)}>
+                    <option value="" disabled>Select a category</option>
+                    <option value="redux">Redux</option>
+                    <option value="react">React</option>
+                    <option value="udacity">Udacity</option>
+                </select>
+
+                <button type="submit">salvar</button>
+            </form>
+
         </div>);
     }
 }
