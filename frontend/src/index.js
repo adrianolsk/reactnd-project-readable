55ac3db9f5336c1587fc12b4f221b44f4450522f
adrianolsk <adriano.skroch@gmail.com>
2017-11-09 13:12:44 -0200

    feat: add redux-logger middleware

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './Readable.css';
-// import './index.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter} from 'react-router-dom';
-import {createStore, applyMiddleware, compose} from 'redux';
+import {applyMiddleware, compose, createStore} from 'redux';
 import thunk from 'redux-thunk';
 import {Provider} from 'react-redux';
 import reducer from './reducers';
+import logger from 'redux-logger'
 
+const middlewares = [
+    logger,
+    thunk
+];
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(reducer, composeEnhancers(applyMiddleware(...middlewares)));
 
 ReactDOM.render(<Provider store={store}>
         <BrowserRouter>
